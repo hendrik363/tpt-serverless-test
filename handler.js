947e3b51f3f2100fd76d363ffe7ab6e4a4bbb4a4
Siim Tiilen <eritikass@gmail.com>
2020-04-22 13:53:23 +0300

    use serverless-http to proxy for express

--- handler.js
+++ handler.js
@@ -1,28 +1,11 @@
 'use strict';
 
-module.exports.hello = async event => {
-  let { queryStringParameters } = event;
-  if (!queryStringParameters) {
-    queryStringParameters = {};
-  }
-  const a = parseInt(queryStringParameters.a, 10) || 0;
-  const b = parseInt(queryStringParameters.b, 10) || 0;
+const serverless = require('serverless-http');
+const express = require('express')
+const app = express()
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        a,
-        b,
-        sum: a + b,
-        // message: 'Go Serverless v1.0! Your function executed successfully!',
-        // input: event,
-      },
-      null,
-      2
-    ),
-  };
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+module.exports.handler = serverless(app);
